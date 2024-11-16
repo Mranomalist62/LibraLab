44ee4e93f13f761141d385bfec1b2e2a5f2e4516
mise en scene <139216558+piezzy@users.noreply.github.com>
2024-11-16 22:45:09 +0700

    first commit

---
+++ libralab-api/frontend/js/script.js
@@ -0,0 +1,47 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const dropdowns = document.querySelectorAll('.dropdown');
+//   const mainContent = document.querySelector('body'); // Pilih seluruh konten body
+
+//   dropdowns.forEach((dropdown) => {
+//     // Event ketika dropdown dibuka
+//     dropdown.addEventListener('show.bs.dropdown', function () {
+//       // Cek apakah dropdown yang diklik
+//       mainContent.classList.add('blur');
+//     });
+
+//     // Event ketika dropdown ditutup
+//     dropdown.addEventListener('hide.bs.dropdown', function () {
+//       mainContent.classList.remove('blur');
+//     });
+//   });
+// });
+
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 4, // Jumlah slide yang ditampilkan sekaligus
+    slidesToScroll: 1,
+    arrows: true, // Tampilkan tombol navigasi
+    dots: true, // Tampilkan navigasi dot
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768, // Pengaturan responsif untuk ukuran layar lebih kecil
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
