abd5d4af106f2f62cd98ceb227f3988f10651b18
Mario <mariofirdaus62@gmail.com>
2024-11-19 02:02:19 +0700

    editing dotenv implementation to ES6 Standart

--- libralab-api/index.js
+++ libralab-api/index.js
@@ -1,7 +1,6 @@
+import 'dotenv/config'
 import express from 'express';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
 
 app.listen(3000, () => console.log('server started'));
